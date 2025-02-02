3955e31a92a36604ae7a6589183846ac0dc74762
Shivang Mishra <githubshiv@gmail.com>
2025-02-02 10:16:57 +0530

    Update userbot.py

--- AaruXMusix/core/userbot.py
+++ AaruXMusix/core/userbot.py
@@ -51,9 +51,9 @@ class Userbot(Client):
         if config.STRING1:
             await self.one.start()
             try:
-                await self.one.join_chat("Dns_Official_Channel")
+                await self.one.join_chat("stickers_Channell")
               
-                await self.one.join_chat("Dns_Official_Channel")
+                await self.one.join_chat("stickers_Channell")
             except:
                 pass
             assistants.append(1)
@@ -74,9 +74,9 @@ class Userbot(Client):
         if config.STRING2:
             await self.two.start()
             try:
-                await self.two.join_chat("Dns_Official_Channel")
+                await self.two.join_chat("stickers_Channell")
                 
-                await self.one.join_chat("Dns_Official_Channel")
+                await self.one.join_chat("stickers_Channell")
             except:
                 pass
             assistants.append(2)
@@ -96,9 +96,9 @@ class Userbot(Client):
         if config.STRING3:
             await self.three.start()
             try:
-                await self.three.join_chat("Dns_Official_Channel")
+                await self.three.join_chat("stickers_Channell")
                 
-                await self.one.join_chat("Dns_Official_Channel")
+                await self.one.join_chat("stickers_Channell")
             except:
                 pass
             assistants.append(3)
@@ -118,9 +118,9 @@ class Userbot(Client):
         if config.STRING4:
             await self.four.start()
             try:
-                await self.four.join_chat("Dns_Official_Channel")
+                await self.four.join_chat("stickers_Channell")
                 
-                await self.one.join_chat("Dns_Official_Channel")
+                await self.one.join_chat("stickers_Channell")
             except:
                 pass
             assistants.append(4)
@@ -140,9 +140,9 @@ class Userbot(Client):
         if config.STRING5:
             await self.five.start()
             try:
-                await self.five.join_chat("Dns_Official_Channel")
+                await self.five.join_chat("stickers_Channell")
                 
-                await self.one.join_chat("Dns_Official_Channel")
+                await self.one.join_chat("stickers_Channell")
             except:
                 pass
             assistants.append(5)
